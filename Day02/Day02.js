95ae4aaaaa2efb1517331144611d764e8f2b85a9
Sean Cole <seancoledesign@gmail.com>
2024-12-12 16:09:58 -0500

    working on Day 2 Part 2

--- Day02/Day02.js
+++ Day02/Day02.js
@@ -1,13 +1,12 @@
-var fs = require('fs');
+fs = require('fs')
 
 fs.readFile('./Day02/input.txt', 'utf-8', (err, data) => {
-    const arr = data.split('\n').map((row) => row.split(' ').map((item) => parseInt(item)))
-    console.log(arr);
+    const arr = data.split('\n').map((row) => row.split(' ').map((item) => parseInt(item)));
     let safeMap = {
-        'safe': -1,
+        'safe': 0,
         'unsafe': 0
     }
-    arr.map((row) => {
+    function isSafe(row){
         let isSafe = true;
         let status = {
             'increasing': undefined,
@@ -16,7 +15,7 @@ fs.readFile('./Day02/input.txt', 'utf-8', (err, data) => {
         for (let i=0;i<row.length-1;i++){
                 if ((row[i] - row[i+1] == 0) || (Math.abs(row[i]-row[i+1]) > 3)){
                     isSafe = false;
-                    break
+                    break;
                 }
                 if (row[i] > row[i+1]){
                     status.decreasing = true;
@@ -29,13 +28,32 @@ fs.readFile('./Day02/input.txt', 'utf-8', (err, data) => {
                 }
                 
         }
-        if(isSafe == false){
+    
+        return isSafe;
+    }
+    function problemDampener(row){
+        if (isSafe(row) == false){
+            for (let i = 0; i < row.length; i++){
+                if (isSafe(row.splice(i,1)) == true){
+                    safeMap.safe += 1;
+                    safeMap.unsafe -= 1;
+                    break;
+                }
+            }
+        }
+    }
+    arr.map((row) => {
+        if(isSafe(row)){
+            safeMap.safe += 1;
+    }
+        else {
             safeMap.unsafe += 1;
         }
-        else {
-            safeMap.safe += 1;
-        }
-       
+    problemDampener(row)
     })
-    console.log(safeMap)
-})    
+    console.log(
+        `Safety Testing Results: 
+    Safe: ${safeMap.safe}
+    Unsafe: ${safeMap.unsafe}`)})
+
+
