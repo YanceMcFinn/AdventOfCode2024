0bb8ae48017b8b17d47343d2d572f050e10ee4cc
Sean Cole <seancole@Seans-MacBook-Pro.local>
2025-01-02 21:43:43 -0500

    file cleanup

--- .history/Day05/Day05_20241224204243.js
+++
@@ -1,8 +0,0 @@
-fs = require('fs')
-
-fs.readFile('./Day05/input.txt', 'utf-8', (err, data) => {
-    const arr = data.split('\n').map((item) => {
-        
-    })
-    console.log(typeof arr[0])
-})
--- .history/Day05/Day05_20241224204325.js
+++
@@ -1,6 +0,0 @@
-fs = require('fs')
-
-fs.readFile('./Day05/input.txt', 'utf-8', (err, data) => {
-    const arr = data.split('\n')
-    console.log(typeof arr[0])
-})
